47369a744283b337da0e9c4ab428775e8fe37ba5
Anton <renooise@gmail.com>
2019-07-30 11:11:07 +0200

    Удален шедулер apsheduler

--- aob.py
+++ aob.py
@@ -1,17 +1,5 @@
 from app import create_app, db, cli
 from app.models import User, Post, Message, Notification, Task
-from apscheduler.schedulers.background import BackgroundScheduler
-from app import tasks
-import atexit
-
-
-def test():
-    tasks.download_tanks_info(1)
-
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=test, trigger="interval", minutes=7)
-scheduler.start()
 
 app = create_app()
 cli.register(app)
